0b529c36f0c29d8a2d9b637820b4dae51de26e80
Greg Baugues <greg@baugues.com>
2014-12-17 21:44:20 -0600

    Carols working

---
+++ twilio.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require 'haml'
+
+SONGS = %w{
+  away-in-a-manger.txt
+  do-you-hear-what-i-hear.txt
+  frosty-the-snowman.txt
+  hark-the-harald-angels-sing.txt
+  have-yourself-a-merry-little-christmas.txt
+  here-comes-santa-claus.txt
+  it-came-upon-the-midnight-clear.txt
+  jingle-bells.txt
+  joy-to-the-world.txt
+  let-it-snow.txt
+  little-drummer-boy.txt
+  o-christmas-tree.txt
+  o-come-all-ye-faithful.txt
+  rudolph-the-red-nosed-reindeer.txt
+  silent-night.txt
+  silver-bells.txt
+  the-first-noel.txt
+  winter-wonderland.txt }
+
+post '/message' do
+  puts song_options
+  if song_options.include?(params['Body'])
+    option = params['Body'].to_i - 1
+    filename = SONGS[option]
+  else
+    filename = 'menu.txt'
+  end
+
+  text = File.read("lyrics/#{filename}")
+  content_type 'text/xml'
+  "
+<Response>
+  <Message>
+    #{text}
+  </Message>
+</Response>"
+end
+
+def song_options
+  (1..SONGS.size).collect { |i| i.to_s }
+end
+
+
